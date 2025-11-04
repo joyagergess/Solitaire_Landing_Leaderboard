61a76957687d6612fdb6e758321761e3cdb684c0
joyagergess <gergessjoya@gmail.com>
2025-11-04 15:37:48 +0200

    Added the code to connection.php and the database is connected perfectly

---
+++ Backend/connection.php
@@ -0,0 +1,8 @@
+<?php 
+$connection = new mysqli ("localhost", "root","","solitaire_db");
+
+if ($connection -> connect_error){
+    die ("connection error:" . $connection-> connect_error);
+}
+echo "Database is connected";
+?>

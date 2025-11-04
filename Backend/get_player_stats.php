9a178c3a750f659194dc9e4c441e869815b0001c
joyagergess <gergessjoya@gmail.com>
2025-11-04 16:21:16 +0200

    Converted array into json format

--- Backend/get_player_stats.php
+++ Backend/get_player_stats.php
@@ -6,3 +6,9 @@ $sql = "SELECT * FROM Leaderboard ORDER BY score DESC" ;
 $res= $connection->query($sql);
 
 $players = $res->fetch_all(MYSQLI_ASSOC);
+
+header('Content-Type : application/json');
+
+echo json_encode($players);
+
+?>

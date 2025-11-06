858421d0eb0b2abe4b7b223deac2c1f3789c4131
joyagergess <gergessjoya@gmail.com>
2025-11-06 20:29:13 +0200

    Fixed javascript functions

--- Backend/connection.php
+++ Backend/connection.php
@@ -1,4 +1,9 @@
 <?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json; charset=UTF-8");
+
 $connection = new mysqli ("localhost", "root","","solitaire_db");
 
 if ($connection -> connect_error){
--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,58 +1,65 @@
+const BASE_URL = "http://localhost/Solitaire_Landing_Leaderboard/Backend/";
 
-function loadLeaderboard() {
-  axios.get('../Backend/get_player_stats.php')
-     .then(function(response) {
-      var players = response.data;
-      var test = "o";
-      var tbody = document.querySelector('#leaderboard tbody');
-      tbody.innerHTML = '';
+async function loadLeaderboard() {
+  try {
+    const url = BASE_URL + "get_player_stats.php";
+    const response = await axios.get(url);
+    const players = response.data;
+    const tbody = document.querySelector('#leaderboard tbody');
+    tbody.innerHTML = '';
 
-     for (var i = 0; i < players.length; i++) {
-      var row = '<tr>' +
-                  '<td>' + players[i].name + '</td>' +
-                  '<td>' + players[i].score + '</td>' +
-                  '<td>' + players[i].duration + '</td>' +
-                  '<td>Top ' + (i + 1) + '</td>' +
-                '</tr>';
-        tbody.innerHTML += row;
-      }
-    });
+    for (let i = 0; i < players.length; i++) {
+      const row = `
+        <tr>
+          <td>${players[i].name}</td>
+          <td>${players[i].score}</td>
+          <td>${players[i].duration}</td>
+          <td>Top ${i + 1}</td>
+        </tr>
+      `;
+      tbody.innerHTML += row;
+    }
+
+  } catch (error) {
+    console.log("error");
+  }
 }
 
 loadLeaderboard();
 
-submit_button.addEventListener('click', function() {
-  var name = name_input.value.trim();
-  var namePattern = /^[A-Za-z\s]+$/;
-
+submit_button.addEventListener('click', async () => {
+  const name = name_input.value.trim();
+  const namePattern = /^[A-Za-z\s]+$/;
+  
   if (name === '') {
     message.textContent = "Enter your name";
     message.style.color = "red";
     return;
   }
 
-    if (!namePattern.test(name)) {
+  if (!namePattern.test(name)) {
     message.textContent = "Name can only contain letters and spaces";
     message.style.color = "red";
     return;
   }
 
-  axios.post('../Backend/add_score.php', { name: name })
-    .then(function(response) {
-      var data = response.data; 
-      message.textContent = data.message;
+  try {
+    const url= BASE_URL +"add_score.php";
+    const response = await axios.post(url, { name });
+    const data = response.data;
 
-      if (data.success) {
-        message.style.color = "green";
-        name_input.value = "";
-        loadLeaderboard();
-      } else {
-        message.style.color = "red";
-      }
-    })
-    .catch(function(error) {
-      console.log(error);
-      message.textContent = "There is an error";
+    message.textContent = data.message;
+
+    if (data.success) {
+      message.style.color = "green";
+      name_input.value = "";
+      loadLeaderboard();
+    } else {
       message.style.color = "red";
-    });
+    }
+  } catch (error) {
+    console.log(error);
+    message.textContent = "There is an error";
+    message.style.color = "red";
+  }
 });

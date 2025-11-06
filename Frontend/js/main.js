7051b26378ebee8d7781d424462cd57a024c512f
joyagergess <gergessjoya@gmail.com>
2025-11-06 15:06:24 +0200

    Refresh

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,17 +1,22 @@
-axios.get('../Backend/get_player_stats.php')
-  .then(function(response) {
-    var players = response.data;
-    var tbody = document.querySelector('#leaderboard tbody');
-    tbody.innerHTML = '';
 
-    for (var i = 0; i < players.length; i++) {
+function loadLeaderboard() {
+  axios.get('../Backend/get_player_stats.php')
+     .then(function(response) {
+      var players = response.data;
+      var tbody = document.querySelector('#leaderboard tbody');
+      tbody.innerHTML = '';
+
+     for (var i = 0; i < players.length; i++) {
       var row = '<tr>' +
                   '<td>' + players[i].name + '</td>' +
                   '<td>' + players[i].score + '</td>' +
                   '<td>' + players[i].duration + '</td>' +
                   '<td>Top ' + (i + 1) + '</td>' +
                 '</tr>';
-      tbody.innerHTML += row;
-    }
-  })
-  
+         tbody.innerHTML += row;
+      }
+    });
+}
+
+loadLeaderboard();
+
